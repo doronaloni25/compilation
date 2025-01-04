39f58f5028eaac8c277cff1663f949bfbf9fe8e0
dannybroyd <danielbroyd@mail.tau.ac.il>
2025-01-04 17:16:24 +0200

    Update HelperUtils.java

--- ex3/src/HelperUtils.java
+++ ex3/src/HelperUtils.java
@@ -5,14 +5,11 @@ import AST.*;
 
 public static boolean isEnhiritedFromOrNil(TYPE t1, TYPE t2) {
     // Check if t1 is enherited from t2
-    if (t1 == null || t2 == null) {
-        return false;
-    }
     if (t1 == t2) {
         return true;
     }
     if (t1 == TYPE_NIL.getInstance()) {
-        return false;
+        return true;
     }
     if (t1.isClass()) {
         return isEnhiritedFromOrNil(((TYPE_CLASS) t1).father, t2);
