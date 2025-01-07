154c37e3fa85f52cb4a8bbb9a7b050659985248a
dannybroyd <danielbroyd@mail.tau.ac.il>
2025-01-07 16:02:50 +0200

    Update AST_VAR_SIMPLE.java

--- ex3/src/AST/AST_VAR_SIMPLE.java
+++ ex3/src/AST/AST_VAR_SIMPLE.java
@@ -51,10 +51,10 @@ public class AST_VAR_SIMPLE extends AST_VAR
 		// if the variable is accessed within class, need to make sure it is defined
 		TYPE_CLASS_DEC classDec = SYMBOL_TABLE.getInstance().inClass();
 		if (classDec != null){
-			TYPE_CLASS_VAR_DEC fieldDec = classDec.fieldInClass(name);
-			TYPE type = SYMBOL_TABLE.getInstance().find(name) 
+			TYPE_CLASS_VAR_DEC fieldDec = classDec.fieldInClass(name); 
 			if (fieldDec == null){
 				// Attempt finding name in global scope if not found in class's ancestors scopes
+				TYPE type = SYMBOL_TABLE.getInstance().find(name);
 				if(type == null) {
 					//TODO- throw exception, the identifier is not defined
 				}
