c9c43f9ce7bd38dad0418bc83d8a2ad45f6f8a84
IritVain <iritvainzof@mail.tau.ac.il>
2025-04-10 15:41:51 +0300

    call func and call method and func dec updated

--- ex5/src/IR/IRcommand_Call_Method.java
+++ ex5/src/IR/IRcommand_Call_Method.java
@@ -58,5 +58,39 @@ public class IRcommand_Call_Method extends IRcommand
             this.dest.setRegisterNumber(interference_graph_map);
         }
 	}
+    public void MIPSme()
+    {
+        TEMP v0 = new TEMP("v",0);
+        TEMP sp = new TEMP("sp",-1);
+        TEMP s0 = new TEMP("s",0);
+        TEMP s1 = new TEMP("s",1);
+        //check null reference
+        MIPSGenerator.getInstance().checkNullPointer(this.object);
+        //load arguments to the stack
+        MIPSGenerator.getInstance().subu(sp,sp, 4*this.funcArgs.size()); 
+        for (int i = 0; i < this.funcArgs.size(); i++){
+            TEMP arg = this.funcArgs.get(i);
+            MIPSGenerator.getInstance().sw(arg,4*i,sp);
+        }
+
+        //get the adress of the method in the class methods table
+        MIPSGenerator.getInstance().subu(sp,sp,4);
+        MIPSGenerator.getInstance().sw(this.object, 0, sp);
+        MIPSGenerator.getInstance().lw(s0, 0, this.object);
+        //s0 has the adress of the class dispach vector
+        int offset = this.offset * 4;
+        //s1 has the adress of the method in the class dispach vector
+        MIPSGenerator.getInstance().lw(s1, offset,s0);
+        //jump to the mehod(using the adress in the register)
+        MIPSGenerator.getInstance().jalr(s1);
+        //discard the arguments from the stack
+        MIPSGenerator.getInstance().addToStack(4*this.funcArgs.size(),sp);
+        //load the return value to the destination register
+        if(this.dest != null)
+        {
+            MIPSGenerator.getInstance().move(this.dest, v0);
+        }
     
+    
+}
 }
